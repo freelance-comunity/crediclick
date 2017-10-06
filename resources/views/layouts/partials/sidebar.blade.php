d221e53115923aa2f3de6bdcdc9e342ec99dce2e
jcmontejo <jncrlsmontejo@gmail.com>
2017-10-06 12:19:45 -0500

    tercer commit crediclick

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -32,7 +32,7 @@
       <li class="treeview">
         <a href="#"><i class='fa fa-cubes'></i><span> Administración</span> <i class="fa fa-angle-left pull-right"></i></a>
         <ul class="treeview-menu">
-          <li><a href="{{ route('rosters.store') }}">Sueldos</a></li>
+          {{-- <li><a href="{{ route('rosters.store') }}">Sueldos</a></li> --}}
           <li><a href="{{ url('expenses-admin') }}">Gastos</a></li>
         </ul>
       </li>

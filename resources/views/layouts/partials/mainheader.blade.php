472f810824abab4f70d1d9e81481d21dd9fdd0ec
jcmontejo <jncrlsmontejo@gmail.com>
2017-10-06 12:44:09 -0500

    cuarto commit crediclick

--- resources/views/layouts/partials/mainheader.blade.php
+++ resources/views/layouts/partials/mainheader.blade.php
@@ -4,9 +4,9 @@
     <!-- Logo -->
     <a href="{{ url('/home') }}" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>C</b>E</span>
+        <span class="logo-mini"><b>C</b>K</span>
         <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>CREDI</b>EFECTIVO </span>
+        <span class="logo-lg"><b>CREDI</b>CLICK </span>
     </a>
 
     <!-- Header Navbar -->
